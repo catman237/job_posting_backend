aaa72409b0fcacc319d3d4a5872cbbe899a7e173
Greg <Ghughes610@gmail.com>
2021-06-24 13:51:31 -0600

    no footer

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,26 @@
 class JobsController < ApplicationController
     
     def index
-        # claas job class to get all the jobs its kinda like @@all
+        # claas job class to get all the jobs, like @@all
         jobs = Job.all 
         render json: jobs
+    end
+
+    def create
+    
+        # this finds the job board that the job will belong too
+        job_board1 = JobBoard.find_by(name: params[:cohort])
+
+        newJob = Job.create(
+        name: params[:name],
+        date: params[:date],
+        job_board_id: job_board1.id,
+        number_of_applications: params[:number_of_applications],
+        number_of_interviews: params[:number_of_interviews],
+        state: params[:state],
+        salary: params[:salary]
+        )
+        render json: newJob
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,9 @@ JobBoard.destroy_all
 jobBoard1 = JobBoard.create(name: '404')
 jobBoard2 = JobBoard.create(name: 'Hack Street')
 
-job1 =Job.create(name: 'Danny', date: '09-01-2022', number_of_interviews: 20, state: 'PA', salary: 100000, number_of_applications: 40, job_board: jobBoard1 )
+job1 =Job.create(name: 'Danny', date: '09-01-2022', number_of_interviews: 20, state: 'NV', salary: 120000, number_of_applications: 40, job_board: jobBoard1 )
+job2 =Job.create(name: 'Chris', date: '09-01-2022', number_of_interviews: 20, state: 'CA', salary: 85000, number_of_applications: 30, job_board: jobBoard1 )
+job3 =Job.create(name: 'Jay', date: '09-01-2022', number_of_interviews: 20, state: 'OH', salary: 100000, number_of_applications: 15, job_board: jobBoard1 )
+job4 =Job.create(name: 'Mandy', date: '09-01-2022', number_of_interviews: 20, state: 'ID', salary: 90000, number_of_applications: 24, job_board: jobBoard1 )
+job5 =Job.create(name: 'Tony', date: '09-01-2022', number_of_interviews: 20, state: 'CO', salary: 80000, number_of_applications: 10, job_board: jobBoard1 )
+job6 =Job.create(name: 'Greg', date: '09-01-2022', number_of_interviews: 20, state: 'AL', salary: 65000, number_of_applications: 20, job_board: jobBoard1 )
